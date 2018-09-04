aefa26fba08e5e3c3c6832cc5bc0d07cc2632705
Boaz Rosenan <brosenan@gmail.com>
2018-09-04 21:40:14 +0300

    frontend-test

--- src/lambdakube_example/core.clj
+++ src/lambdakube_example/core.clj
@@ -9,50 +9,53 @@
        (into {})))
 
 (defn module [$]
-  (-> $
-      (lk/rule :backend-master []
-               (fn []
-                 (-> (lk/pod :redis-master {:app :redis
-                                            :role :master
-                                            :tier :backend})
-                     (lk/add-container :redis "redis:5.0-rc"
-                                       {:resources {:requests {:cpu "100m"
-                                                               :memory "100Mi"}}})
-                     (lk/deployment 1)
-                     (lk/expose-cluster-ip :redis-master
-                                           (lk/port :redis :redis 6379 6379)))))
-      (lk/rule :backend-slave [:backend-master :num-be-slaves]
-               (fn [backend-master num-be-slaves]
-                 (-> (lk/pod :redis-slave {:app :redis
-                                           :role :slave
-                                           :tier :backend})
-                     (lk/add-container :redis "redis:5.0-rc"
-                                       {:resources {:requests {:cpu "100m"
-                                                               :memory "100Mi"}}
-                                        :args ["--slaveof"
-                                               (:hostname backend-master)
-                                               (-> backend-master :ports :redis str)]})
-                     (lk/deployment num-be-slaves)
-                     (lk/expose-cluster-ip :redis-slave
-                                           (lk/port :redis :redis 6379 6379)))))
-      (lk/rule :frontend [:backend-master :backend-slave :num-fe-replicas]
-               (fn [master slave num-replicas]
-                 ;; We start with an empty pod.
-                 (-> (lk/pod :frontend {:app :guesbook
-                                        :tier :frontend})
-                     ;; We add a container, specifying a name, image and environments.
-                     (lk/add-container :php-redis "gcr.io/google-samples/gb-frontend:v4"
-                                       (lk/add-env {}  {:GET_HOST_FROM :dns}))
-                     ;; We load three files from resources and mount them to the container
-                     (lk/add-files-to-container :php-redis :new-gb-fe-files "/var/www/html"
-                                                (map-resources ["index.html" "controllers.js" "guestbook.php"]))
-                     ;; Wait for the master and slave to come up
-                     (lku/wait-for-service-port master :redis)
-                     (lku/wait-for-service-port slave :redis)
-                     ;; Then we wrap the pod with a deployment, specifying the number of replicas.
-                     (lk/deployment num-replicas)
-                     ;; Finally, we expose port 80 using a NodePort service.
-                     (lk/expose-node-port :frontend (lk/port :php-redis :web 80)))))))
+  (let [redis-image "redis:5.0-rc"
+        redis-res {:requests {:cpu "100m"
+                              :memory "100Mi"}}]
+    (-> $
+        (lk/rule :backend-master []
+                 (fn []
+                   (-> (lk/pod :redis-master {:app :redis
+                                              :role :master
+                                              :tier :backend})
+                       (lk/add-container :redis redis-image
+                                         {:resources redis-res})
+                       (lk/deployment 1)
+                       (lk/expose-cluster-ip :redis-master
+                                             (lk/port :redis :redis 6379 6379)))))
+        (lk/rule :backend-slave [:backend-master :num-be-slaves]
+                 (fn [backend-master num-be-slaves]
+                   (-> (lk/pod :redis-slave {:app :redis
+                                             :role :slave
+                                             :tier :backend})
+                       (lk/add-container :redis redis-image
+                                         {:resources redis-res
+                                          :args ["--slaveof"
+                                                 (:hostname backend-master)
+                                                 (-> backend-master :ports :redis str)]})
+                       (lk/deployment num-be-slaves)
+                       (lk/expose-cluster-ip :redis-slave
+                                             (lk/port :redis :redis 6379 6379)))))
+        (lk/rule :frontend [:backend-master :backend-slave :num-fe-replicas]
+                 (fn [master slave num-replicas]
+                   ;; We start with an empty pod.
+                   (-> (lk/pod :frontend {:app :guesbook
+                                          :tier :frontend})
+                       ;; We add a container, specifying a name, image and environments.
+                       (lk/add-container :php-redis "gcr.io/google-samples/gb-frontend:v4"
+                                         (lk/add-env {} {:GET_HOST_FROM :env
+                                                         :REDIS_MASTER_SERVICE_HOST (:hostname master)
+                                                         :REDIS_SLAVE_SERVICE_HOST (:hostname slave)}))
+                       ;; We load three files from resources and mount them to the container
+                       (lk/add-files-to-container :php-redis :new-gb-fe-files "/var/www/html"
+                                                  (map-resources ["index.html" "controllers.js" "guestbook.php"]))
+                       ;; Wait for the master and slave to come up
+                       (lku/wait-for-service-port master :redis)
+                       (lku/wait-for-service-port slave :redis)
+                       ;; Then we wrap the pod with a deployment, specifying the number of replicas.
+                       (lk/deployment num-replicas)
+                       ;; Finally, we expose port 80 using a NodePort service.
+                       (lk/expose-cluster-ip :frontend (lk/port :php-redis :web 80 80))))))))
 
 (def config
   {:num-be-slaves 3
--- test/lambdakube_example/core_test.clj
+++ test/lambdakube_example/core_test.clj
@@ -5,6 +5,7 @@
            [lambdakube.util :as lku]
            [lambdakube.testing :as lkt]))
 
+
 (defn test-module [$]
   (-> $
       (lkt/test :redis-slave-configured
@@ -14,25 +15,62 @@
                   (-> (lk/pod :test {})
                       (lku/wait-for-service-port master :redis)
                       (lku/wait-for-service-port slave :redis)
-                      (lku/add-midje-container :test
-                                               '[[org.clojure/clojure "1.8.0"]
-                                                 [com.taoensso/carmine "2.18.1"]]
-                                               '[(ns main-test
-                                                   (require [midje.sweet :refer :all]
-                                                            [taoensso.carmine :as car]))
-                                                 (def master-conn {:pool {} :spec {:host "redis-master"
-                                                                                   :port 6379}})
-                                                 (def slave-conn {:pool {} :spec {:host "redis-slave"
-                                                                                  :port 6379}})
-                                                 (fact
-                                                  (car/wcar master-conn
-                                                            (car/with-replies
-                                                              (car/set "foo" "bar")) => "OK")
-                                                  ;; Wait for value to propagate to slave
-                                                  (Thread/sleep 100)
-                                                  (car/wcar slave-conn
-                                                            (car/with-replies
-                                                              (car/get "foo")) => "bar"))]))))))
+                      (lku/add-midje-container
+                       :test
+                       '[[org.clojure/clojure "1.8.0"]
+                         [com.taoensso/carmine "2.18.1"]]
+                       '[(ns main-test
+                           (:require [midje.sweet :refer :all]
+                                     [taoensso.carmine :as car]))
+                         (def master-conn {:pool {} :spec {:host "redis-master"
+                                                           :port 6379}})
+                         (def slave-conn {:pool {} :spec {:host "redis-slave"
+                                                          :port 6379}})
+                         (fact
+                          (car/wcar master-conn
+                                    (car/with-replies
+                                      (car/set "foo" "bar")) => "OK")
+                          ;; Wait for value to propagate to slave
+                          (Thread/sleep 1000)
+                          (car/wcar slave-conn
+                                    (car/with-replies
+                                      (car/get "foo")) => "bar"))]))))
+      (lkt/test :frontend-set-and-get
+                {:num-be-slaves 1
+                 :num-fe-replicas 1}
+                [:frontend]
+                (fn [frontend]
+                  (-> (lk/pod :test {})
+                      (lku/wait-for-service-port frontend :web)
+                      (lku/add-midje-container
+                       :test
+                       '[[org.clojure/clojure "1.8.0"]
+                         [clj-http "3.9.1"]]
+                       '[(ns main-test
+                           (:require [midje.sweet :refer :all]
+                                     [clj-http.client :as client]
+                                     [clojure.string :as str]))
+                         (defn wget [query]
+                           (let [base-url (System/getenv "FE_BASE_URL")
+                                 resp (client/get (str base-url "?" (str/join "&" (for [[k v] query]
+                                                                                    (str k "=" v)))))]
+                             (when-not (= (:status resp) 200)
+                               (throw (Exception. (str "Bad status" (:status resp) ": " (:body resp)))))
+                             (:body resp)))
+                         (fact
+                          (wget {"cmd" "set"
+                                 "key" "foo"
+                                 "value" "bar"}) => "{\"message\": \"Updated\"}")
+                         (fact
+                          (wget {"cmd" "get"
+                                 "key" "foo"}) => "{\"data\": \"bar\"}")])
+                      (lk/update-container :test lk/add-env {"FE_BASE_URL" (str "http://"
+                                                                                (-> frontend :hostname name)
+                                                                                ":"
+                                                                                (-> frontend :ports :web)
+                                                                                "/guestbook.php")})
+                      (update-in [:spec :initContainers 0] assoc :command
+                                 ["sh" "-c" "while ! nc -z frontend 80; do sleep 1; done"]))))))
 
 (deftest kubetests
   (is (= (-> (lk/injector)
